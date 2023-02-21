ffc036136a85ccc69f606643cfd7bb5900979b6f
Maurits <mcb.westbroek@gmail.com>
2023-02-21 07:35:24 +0000

    Make script specific to launching cluster

--- launch_cluster.py
+++ launch_cluster.py
@@ -14,11 +14,11 @@ TOKEN=config.get("AWS", "TOKEN")
 # IAM
 DWH_IAM_ROLE=config.get("IAM_ROLE", "DWH_IAM_ROLE")
 # REDSHIFT
-CLUSTER_TYPE=config.get("REDSHIFT", "CLUSTER_TYPE")
-NODE_TYPE=config.get("REDSHIFT", "NODE_TYPE")
-NODES=int(config.get("REDSHIFT", "NODES"))
-NODE_TYPE=config.get("REDSHIFT", "NODE_TYPE")
-CLUSTER_ID=config.get("REDSHIFT", "CLUSTER_ID")
+CLUSTER_TYPE=config.get("CLUSTER", "CLUSTER_TYPE")
+NODE_TYPE=config.get("CLUSTER", "NODE_TYPE")
+NODES=int(config.get("CLUSTER", "NODES"))
+CLUSTER_ID=config.get("CLUSTER", "CLUSTER_ID")
+
 DB=config.get("REDSHIFT", "DB")
 USER=config.get("REDSHIFT", "USER")
 PWD=config.get("REDSHIFT", "PWD")
@@ -48,23 +48,27 @@ iam=boto3.client(
     aws_session_token=TOKEN)
 
 # create role for Redshift
-dwhRole = iam.create_role(
-Path='/',
-RoleName=DWH_IAM_ROLE,
-AssumeRolePolicyDocument=json.dumps(
-    {
-        "Version": "2012-10-17",
-        "Statement": [
+try:
+    dwhRole = iam.create_role(
+    Path='/',
+    RoleName=DWH_IAM_ROLE,
+    AssumeRolePolicyDocument=json.dumps(
         {
-            "Effect": "Allow",
-            "Principal": {
-            "Service": "redshift.amazonaws.com"
-            },
-            "Action": "sts:AssumeRole"
-        }
-        ]
-    }               
-))
+            "Version": "2012-10-17",
+            "Statement": [
+            {
+                "Effect": "Allow",
+                "Principal": {
+                "Service": "redshift.amazonaws.com"
+                },
+                "Action": "sts:AssumeRole"
+            }
+            ]
+        }               
+    ))
+
+except Exception as e:
+    print(e)
 
 # attach a policy to allow Redshift access to s3
 iam.attach_role_policy(
@@ -73,13 +77,17 @@ iam.attach_role_policy(
 )
 
 # get the role ARN for the new role
-roleArn =iam.get_role(
+roleArn=iam.get_role(
     RoleName=DWH_IAM_ROLE
-)
+)['Role']['Arn']
+
+print(f'The role ARN is {roleArn}')
+
+print('Creating cluster...')
 
 # Launch a cluster
 redshift=boto3.client(
-    'iam', 
+    'redshift', 
     region_name='us-east-1', 
     aws_access_key_id=KEY, 
     aws_secret_access_key=SECRET,
@@ -100,37 +108,3 @@ response = redshift.create_cluster(
     #Roles (for s3 access)
     IamRoles=[roleArn]  
 )
-
-# get cluster properties
-PROPERTIES = redshift.describe_clusters(ClusterIdentifier=CLUSTER_ID)['Clusters'][0]
-# note hostname (endpoint)
-ENDPOINT = PROPERTIES['Endpoint']['Address']
-
-# authorise ingress
-ec2=boto3.resource(
-    'ec2', 
-    region_name='us-east-1', 
-    aws_access_key_id=KEY, 
-    aws_secret_access_key=SECRET,
-    aws_session_token=TOKEN
-    )
-    
-vpc = ec2.Vpc(id=PROPERTIES['VpcId'])
-
-defaultSg = list(vpc.security_groups.all())[0]
-defaultSg.authorize_ingress(
-    GroupName=defaultSg.group_name,
-    CidrIp='0.0.0.0/0',
-    IpProtocol='TCP',
-    FromPort=int(PORT),
-    ToPort=int(PORT)
-)
-
-"""
-# delete - uncomment when needed
-redshift.delete_cluster(ClusterIdentifier=CLUSTER_ID,  SkipFinalClusterSnapshot=True)
-
-# remove IAM roles etc.
-iam.detach_role_policy(RoleName=DWH_IAM_ROLE, PolicyArn="arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess")
-iam.delete_role(RoleName=DWH_IAM_ROLE)
-"""
