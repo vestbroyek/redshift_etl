0f428ba14cfd13fb2e8cdef28956e6ce6ded0b83
Maurits <mcb.westbroek@gmail.com>
2023-02-21 07:35:52 +0000

    Add config

--- create_tables.py
+++ create_tables.py
@@ -1,7 +1,11 @@
 import configparser
+import os
 import psycopg2
 from sql_queries import create_table_queries, drop_table_queries
 
+config = configparser.ConfigParser()
+cfg_path = os.path.join(os.getcwd(),'dwh.cfg')
+config.read(cfg_path)
 
 def drop_tables(cur, conn):
     for query in drop_table_queries:
@@ -19,7 +23,7 @@ def main():
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
-    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['REDSHIFT'].values()))
     cur = conn.cursor()
 
     drop_tables(cur, conn)
